d37038b7b9699e04e77e5477a927232cd8b6936b
coderzzp <coderzzp@gmail.com>
2024-05-18 20:55:27 +0800

    feat: 登录钱包 & 登录eth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,15 +18,14 @@ import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-import { NextPage } from 'next';
-
+ 
 // Configure chains & providers with the Alchemy provider.
 // Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
 
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet],
-  [alchemyProvider({ apiKey: 'alcht_VqpqQaiLboe1VNg4Gs8d9OVRZT6mEP' }), publicProvider()],
+  [alchemyProvider({ apiKey: 'alcht_EWlN0Nh1cs9mQgBdynqTgB0EkisxsU' }), publicProvider()],
 )
  
 // Set up wagmi config
@@ -40,108 +39,41 @@ const config = createConfig({
         appName: 'wagmi',
       },
     }),
-
+    // new WalletConnectConnector({
+    //   chains,
+    //   options: {
+    //     projectId: 'f193d33557bc877a99c624623aae912b',
+    //   },
+    // }),
+    // new InjectedConnector({
+    //   chains,
+    //   options: {
+    //     name: 'Injected',
+    //     shimDisconnect: true,
+    //   },
+    // }),
   ],
   publicClient,
   webSocketPublicClient,
 })
  
 // Pass config to React Context Provider
-const App:NextPage<{}> = () =>{
+function App() {
   return (
     <WagmiConfig config={config}>
       <Profile />
     </WagmiConfig>
   )
 }
-
-
-
-function SignInButton({
-  onSuccess,
-  onError,
-}: {
-  onSuccess: (args: { address: string }) => void
-  onError: (args: { error: Error }) => void
-}) {
-  const [state, setState] = React.useState<{
-    loading?: boolean
-    nonce?: string
-  }>({})
-
-  const fetchNonce = async () => {
-    try {
-      const nonceRes = await fetch('/api/nonce')
-      const nonce = await nonceRes.text()
-      setState((x) => ({ ...x, nonce }))
-    } catch (error) {
-      setState((x) => ({ ...x, error: error as Error }))
-    }
-  }
-
-  // Pre-fetch random nonce when button is rendered
-  // to ensure deep linking works for WalletConnect
-  // users on iOS when signing the SIWE message
-  React.useEffect(() => {
-    fetchNonce()
-  }, [])
-
-  const { address } = useAccount()
-  const { chain } = useNetwork()
-  const { signMessageAsync } = useSignMessage()
-
-  const signIn = async () => {
-    try {
-      const chainId = chain?.id
-      console.log('address',address)
-      console.log('chainId',chainId)
-      if (!address || !chainId) return
-
-      setState((x) => ({ ...x, loading: true }))
-      // Create SIWE message with pre-fetched nonce and sign with wallet
-      const message = new SiweMessage({
-        domain: window.location.host,
-        address,
-        statement: 'Sign in with Ethereum to the app.',
-        uri: window.location.origin,
-        version: '1',
-        chainId,
-        nonce: state.nonce,
-      })
-      const signature = await signMessageAsync({
-        message: message.prepareMessage(),
-      })
-      console.log('signature',signature)
-      console.log('message',message)
-      // Verify signature
-      const verifyRes = await fetch('/api/verify', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message, signature }),
-      })
-      if (!verifyRes.ok) throw new Error('Error verifying message')
-
-      setState((x) => ({ ...x, loading: false }))
-      onSuccess({ address })
-    } catch (error) {
-      setState((x) => ({ ...x, loading: false, nonce: undefined }))
-      onError({ error: error as Error })
-      fetchNonce()
-    }
-  }
-
-  return (
-    <button disabled={!state.nonce || state.loading} onClick={signIn}>
-      Sign-In with Ethereum
-    </button>
-  )
-}
-
-function Profile() {
-  const { isConnected } = useAccount()
+ 
+export function Profile() {
+  const { address, connector, isConnected } = useAccount()
   console.log('isConnected',isConnected)
+  const { data: ensName } = useEnsName({ address })
+  const { data: ensAvatar } = useEnsAvatar({ name: ensName })
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect()
+
   const [state, setState] = React.useState<{
     address?: string
     error?: Error
@@ -192,7 +124,108 @@ function Profile() {
     )
   }
 
-  return <div>{/* Connect wallet content goes here */}</div>
+ 
+  return (
+    <div>
+      {connectors.map((connector) => (
+        <button
+          disabled={!connector.ready}
+          key={connector.id}
+          onClick={() => connect({ connector })}
+        >
+          {connector.name}
+          {!connector.ready && ' (unsupported)'}
+          {isLoading &&
+            connector.id === pendingConnector?.id &&
+            ' (connecting)'}
+        </button>
+      ))}
+ 
+      {error && <div>{error.message}</div>}
+    </div>
+  )
 }
+
+
+function SignInButton({
+  onSuccess,
+  onError,
+}: {
+  onSuccess: (args: { address: string }) => void
+  onError: (args: { error: Error }) => void
+}) {
+  const [state, setState] = React.useState<{
+    loading?: boolean
+    nonce?: string
+  }>({})
+
+  const fetchNonce = async () => {
+    try {
+      const nonceRes = await fetch('/api/nonce')
+      const nonce = await nonceRes.text()
+      setState((x) => ({ ...x, nonce }))
+    } catch (error) {
+      setState((x) => ({ ...x, error: error as Error }))
+    }
+  }
+
+  // Pre-fetch random nonce when button is rendered
+  // to ensure deep linking works for WalletConnect
+  // users on iOS when signing the SIWE message
+  React.useEffect(() => {
+    fetchNonce()
+  }, [])
+
+  const { address } = useAccount()
+  const { chain } = useNetwork()
+  const { signMessageAsync } = useSignMessage()
+
+  const signIn = async () => {
+    try {
+      const chainId = chain?.id
+      if (!address || !chainId) return
+
+      setState((x) => ({ ...x, loading: true }))
+      // Create SIWE message with pre-fetched nonce and sign with wallet
+      const message = new SiweMessage({
+        domain: window.location.host,
+        address,
+        statement: 'Sign in with Ethereum to the app.',
+        uri: window.location.origin,
+        version: '1',
+        chainId,
+        nonce: state.nonce,
+      })
+      const signature = await signMessageAsync({
+        message: message.prepareMessage(),
+      })
+
+      // Verify signature
+      const verifyRes = await fetch('/api/verify', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message, signature }),
+      })
+      if (!verifyRes.ok) throw new Error('Error verifying message')
+
+      setState((x) => ({ ...x, loading: false }))
+      onSuccess({ address })
+    } catch (error) {
+      setState((x) => ({ ...x, loading: false, nonce: undefined }))
+      onError({ error: error as Error })
+      fetchNonce()
+    }
+  }
+
+  return (
+    <button disabled={!state.nonce || state.loading} onClick={signIn}>
+      Sign-In with Ethereum
+    </button>
+  )
+}
+
+
 
 export default App
